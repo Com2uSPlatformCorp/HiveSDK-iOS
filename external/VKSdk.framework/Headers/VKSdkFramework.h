73274821a6a8e46f8619703c72dcc27188fe9c49
sjyong <seokjinyong@gamevilcom2us.com>
2022-07-29 16:35:44 +0900

    #GCPSDK4-820 CocoaPods로 HiveSDK 배포를 위한 podspec 작성. 수동 다운로드로 참조하는 외부 프레임워크들 등록
     - TencentQQ, VK, WeChat

---
+++ external/VKSdk.framework/Headers/VKSdkFramework.h
@@ -0,0 +1,18 @@
+//
+//  VKSdk.h
+//  VKSdk
+//
+//  Created by jhjang on 08/08/2019.
+//  Copyright © 2019 jhjang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VKSdk.
+FOUNDATION_EXPORT double VKSdkVersionNumber;
+
+//! Project version string for VKSdk.
+FOUNDATION_EXPORT const unsigned char VKSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VKSdk/PublicHeader.h>
+#import "VKSdk.h"
---
+++ external/WXApi.framework/Headers/WXApiFramework.h
@@ -0,0 +1,19 @@
+//
+//  WXApi.h
+//  WXApi
+//
+//  Created by jhjang on 08/08/2019.
+//  Copyright © 2019 jhjang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WXApi.
+FOUNDATION_EXPORT double WXApiVersionNumber;
+
+//! Project version string for WXApi.
+FOUNDATION_EXPORT const unsigned char WXApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WXApi/PublicHeader.h>
+
+#import "WXApi.h"
