b917c3f8f2d2b89629d94644a116c1ec00485fbd
sjyong <seokjinyong@gamevilcom2us.com>
2022-08-08 18:15:54 +0900

    GCPSDK4-820 CocoaPods로 HiveSDK 배포를 위한 podspec 작성 - TencentOpenAPI 버전 다운그레이드 하여 배포 시도 (3.3.0)

--- external/TencentOpenAPI/TencentOpenAPI.framework/Headers/TencentOAuth.h
+++ external/TencentOpenAPI/TencentOpenAPI.framework/Headers/TencentOAuth.h
@@ -17,16 +17,18 @@
 @class TencentApiReq;
 @class TencentApiResp;
 
-typedef NS_ENUM(NSUInteger, TencentAuthorizeState) {
+typedef enum
+{
     kTencentNotAuthorizeState,
     kTencentSSOAuthorizeState,
     kTencentWebviewAuthorzieState,
-};
+} TencentAuthorizeState;
 
-typedef NS_ENUM(NSUInteger, TencentAuthMode) {
+typedef enum
+{
     kAuthModeClientSideToken,
     kAuthModeServerSideCode,
-};
+} TencentAuthMode;
 
 #pragma mark - TencentOAuth(授权登录及相关开放接口调用)
 
@@ -68,9 +70,6 @@ typedef NS_ENUM(NSUInteger, TencentAuthMode) {
 /** 第三方应用在互联开放平台申请的appID */
 @property(nonatomic, retain) NSString* appId;
 
-/** 第三方应用在互联开放平台注册的UniversalLink */
-@property(nonatomic, retain) NSString* universalLink;
-
 /** 主要是互娱的游戏设置uin */
 @property(nonatomic, retain) NSString* uin;
 
@@ -88,35 +87,6 @@ typedef NS_ENUM(NSUInteger, TencentAuthMode) {
 
 /** 第三方在授权登录/分享 时选择 QQ，还是TIM 。在授权前一定要指定其中一个类型*/
 @property(nonatomic, assign) TencentAuthShareType authShareType;
-
-/**
- * 获取上次登录得到的token
- *
- **/
-- (NSString *)getCachedToken;
-
-/**
- * 获取上次登录得到的openid
- *
- **/
-- (NSString *)getCachedOpenID;
-
-/**
- * 获取上次登录的token过期日期
- *
- **/
-- (NSDate *)getCachedExpirationDate;
-
-/**
- * 上次登录的token是否过期(本地判断)
- **/
-- (BOOL)isCachedTokenValid;
-
-/**
- * 删除上次登录登录的token信息
- *
- **/
-- (BOOL)deleteCachedToken;
 
 /**
  * 用来获得当前sdk的版本号
@@ -153,75 +123,34 @@ typedef NS_ENUM(NSUInteger, TencentAuthMode) {
  * 用来获得当前手机qq的版本号
  * \return 返回手机qq版本号
  **/
-+ (int)iphoneQQVersion __attribute__((deprecated("已过期, 建议删除调用")));
++ (QQVersion)iphoneQQVersion;
 
 
 /**
  * 用来获得当前手机TIM的版本号
  * \return 返回手机qq版本号
  **/
-+ (int)iphoneTIMVersion __attribute__((deprecated("已过期, 建议删除调用")));
++ (QQVersion)iphoneTIMVersion;
 
 /**
  * 初始化TencentOAuth对象
- * \param appId 不可为nil，第三方应用在互联开放平台申请的唯一标识
- * \param delegate 不可为nil，第三方应用用于接收请求返回结果的委托对象
+ * \param appId 第三方应用在互联开放平台申请的唯一标识
+ * \param delegate 第三方应用用于接收请求返回结果的委托对象
  * \return 初始化后的授权登录对象
  */
 - (id)initWithAppId:(NSString *)appId
         andDelegate:(id<TencentSessionDelegate>)delegate;
 
-/**
-* 初始化TencentOAuth对象（>=3.3.7）
-* \param appId 不可为nil，第三方应用在互联开放平台申请的唯一标识
-* \param universalLink 可以为nil，第三方应用在互联开放平台注册的UniversalLink，和bundleID一一对应（当为nil时，互联平台会按规则生成universallink，详见官网说明）
-* \param delegate 不可为nil，第三方应用用于接收请求返回结果的委托对象
-* \return 初始化后的授权登录对象
-*
-****【使用说明】*****
-* 1、支持BundleId与UniversalLink的一一对应，主要目的“是为了解决应用的iPhone版本和iPad HD版本共用同一个AppId，导致同时安装情况下的跳转问题"。
-* 2 、由于手Q版本在 >=8.1.8 后才支持了这种对应方式，所以一旦使用，“务必做到”及时知会用户升级手Q版本。
-****
-*/
-- (id)initWithAppId:(NSString *)appId
-   andUniversalLink:(NSString *)universalLink
-        andDelegate:(id<TencentSessionDelegate>)delegate;
-
-/**
-* 初始化TencentOAuth对象（>=3.3.8）
-* \param appId 不可为nil，第三方应用在互联开放平台申请的唯一标识
-* \param enabled  默认为NO，第三方应用是否将sdk和手机QQ的交互方式切换为UniversalLink方式，启用后则在iOS9及以上的系统都会生效UniversalLink方式；否则，默认仅在iOS13及以上的系统生效UniversalLink方式。
-* \param universalLink 可以为nil，第三方应用在互联开放平台注册的UniversalLink，和bundleID一一对应（当为nil时，互联平台会按规则生成UniversalLink，详见官网说明）
-* \param delegate 不可为nil，第三方应用用于接收请求返回结果的委托对象
-* \return 初始化后的授权登录对象
-*
-*****【使用说明】*****
-*  1、支持sdk与手Q的交互切换为UniversalLink模式，主要目的"是为了避免手Q的UrlScheme被其他应用抢注后，导致sdk接口功能受到影响"。
-*  2 、由于手Q版本在 >=8.1.3 后才适配了UniversalLink，所以一旦开启了enabled开关，“务必做到”及时知会用户升级手Q版本。
-*****
-*/
-- (id)initWithAppId:(NSString *)appId
- enableUniveralLink:(BOOL)enabled
-      universalLink:(NSString *)universalLink
-           delegate:(id<TencentSessionDelegate>)delegate;
 
 /**
  * 判断用户手机上是否安装手机QQ
  * \return YES:安装 NO:没安装
- *
- * \note SDK目前已经支持QQ、TIM授权登录及分享功能， 会按照QQ>TIM的顺序进行调用。
- * 只要用户安装了QQ、TIM中任意一个应用，都可为第三方应用进行授权登录、分享功能。
- * 第三方应用在接入SDK时不需要判断是否安装QQ、TIM。若有判断安装QQ、TIM的逻辑建议移除。
  */
 + (BOOL)iphoneQQInstalled;
 
 /**
  * 判断用户手机上是否安装手机TIM
  * \return YES:安装 NO:没安装
- *
- * \note SDK目前已经支持QQ、TIM授权登录及分享功能， 会按照QQ>TIM的顺序进行调用。
- * 只要用户安装了QQ、TIM中任意一个应用，都可为第三方应用进行授权登录、分享功能。
- * 第三方应用在接入SDK时不需要判断是否安装QQ、TIM。若有判断安装QQ、TIM的逻辑建议移除。
  */
 + (BOOL)iphoneTIMInstalled;
  
@@ -229,13 +158,13 @@ typedef NS_ENUM(NSUInteger, TencentAuthMode) {
  * 判断用户手机上的手机QQ是否支持SSO登录
  * \return YES:支持 NO:不支持
  */
-+ (BOOL)iphoneQQSupportSSOLogin __attribute__((deprecated("QQ版本均支持SSO登录。该接口已过期, 建议删除调用")));
++ (BOOL)iphoneQQSupportSSOLogin;
 
 /**
  * 判断用户手机上的手机TIM是否支持SSO登录
  * \return YES:支持 NO:不支持
  */
-+ (BOOL)iphoneTIMSupportSSOLogin __attribute__((deprecated("TIM版本均支持SSO登录。该接口已过期, 建议删除调用")));
++ (BOOL)iphoneTIMSupportSSOLogin;
 
 /**
  * 登录授权
@@ -261,13 +190,6 @@ typedef NS_ENUM(NSUInteger, TencentAuthMode) {
 - (BOOL)authorize:(NSArray *)permissions
        localAppId:(NSString *)localAppId
 		 inSafari:(BOOL)bInSafari;
-
-/**
- * 登录授权<web为二维码扫码方式>
- *
- * \param permissions 授权信息列
- */
-- (BOOL)authorizeWithQRlogin:(NSArray *)permissions;
 
 /**
  * 增量授权，因用户没有授予相应接口调用的权限，需要用户确认是否授权
@@ -302,21 +224,6 @@ typedef NS_ENUM(NSUInteger, TencentAuthMode) {
  * \return 处理结果，YES表示可以 NO表示不行
  */
 + (BOOL)CanHandleOpenURL:(NSURL *)url;
-
-/**
- * (静态方法)处理应用的UniversalLink拉起协议
- * \param url 处理被其他应用呼起时的逻辑
- * \return 处理结果，YES表示成功，NO表示失败
- */
-+ (BOOL)HandleUniversalLink:(NSURL *)url;
-
-/**
- * (静态方法)sdk是否可以处理应用的Universallink拉起协议
- * \param url 处理被其他应用呼起时的逻辑(应用的Universallink链接须满足官网注册时的格式要求)
- * \return 处理结果，YES表示可以 NO表示不行
- * 注：在调用其他Universallink相关处理接口之前，均需进行此项判断
- */
-+ (BOOL)CanHandleUniversalLink:(NSURL *)url;
 
 /**
  * (静态方法)获取TencentOAuth调用的上一次错误信息
@@ -412,12 +319,6 @@ typedef NS_ENUM(NSUInteger, TencentAuthMode) {
  * unionID获得
  */
 - (void)didGetUnionID;
-
-/**
- * 强制网页登录，包括账号密码登录和二维码登录
- * return YES时，就算本地有手Q也会打开web界面
- */
-- (BOOL)forceWebLogin;
 @end
 
 #pragma mark - TencentSessionDelegate(开放接口回调协议)
@@ -475,6 +376,7 @@ typedef NS_ENUM(NSUInteger, TencentAuthMode) {
  */
 - (void)getUserInfoResponse:(APIResponse*) response;
 
+
 /**
  * 社交API统一回调接口
  * \param response API返回结果，具体定义参见sdkdef.h文件中\ref APIResponse
@@ -499,7 +401,6 @@ typedef NS_ENUM(NSUInteger, TencentAuthMode) {
  * \param viewController 需要关闭的viewController
  */
 - (void)tencentOAuth:(TencentOAuth *)tencentOAuth doCloseViewController:(UIViewController *)viewController;
-
 @end
 
 #pragma mark - TencentWebViewDelegate(H5登录webview旋转方向回调)
