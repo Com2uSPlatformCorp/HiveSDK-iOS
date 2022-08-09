dfc4bacd15281c49c2b9fad178a6130c677da7a7
sjyong <seokjinyong@gamevilcom2us.com>
2022-08-09 17:51:14 +0900

    GCPSDK4-820 CocoaPods로 HiveSDK 배포를 위한 podspec 작성 - external에 VK 제거. TencentOpenAPI, WXApi 재업로드
     - .h를 public_header_files로 참조 가능하게  하기 위함

---
+++ external/TencentOpenAPI.framework/Headers/TencentOpenAPI.h
@@ -0,0 +1,19 @@
+//
+//  TencentOpenAPI.h
+//  TencentOpenAPI
+//
+//  Created by jhjang on 13/12/2019.
+//  Copyright © 2019 jhjang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TencentOpenAPI.
+FOUNDATION_EXPORT double TencentOpenAPIVersionNumber;
+
+//! Project version string for TencentOpenAPI.
+FOUNDATION_EXPORT const unsigned char TencentOpenAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TencentOpenAPI/PublicHeader.h>
+#import <TencentOpenAPI/TencentOAuth.h>
+
